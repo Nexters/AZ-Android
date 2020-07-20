556d003d3f13fc482c8f8e925957880b322c681b
nukeolaf <yhjin04170@gmail.com>
2020-07-21 07:36:44 +0900

    YTG [샘플][네트워크] Network di 추가

---
+++ data/network/src/main/java/com/olaf/network/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.olaf.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "http://api.github.com/"
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single {
+        get<Retrofit>().create(
+            GithubApi::class.java
+        )
+    }
+}
