a912d18adf243dfb833b888b6aced6a88dfd8b65
HunJin Kim <ysg01129@naver.com>
2020-07-22 23:15:35 +0900

    YTG [샘플][빌드] 코인 빌드 오류 수정

--- data/network/src/main/java/com/olaf/network/NetworkModule.kt
+++ data/network/src/main/java/com/olaf/network/NetworkModule.kt
@@ -9,6 +9,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val BASE_URL = "http://api.github.com/"
 
 val networkModule = module {
+
+    single {
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
     single {
         OkHttpClient.Builder()
             .addInterceptor(get<HttpLoggingInterceptor>())
@@ -18,7 +29,7 @@ val networkModule = module {
     single {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(get())
+            .client(get<OkHttpClient>())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
