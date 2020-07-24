7e49fb55a8edbe31e8a1d04760b143661b66b644
nukeolaf <yhjin04170@gmail.com>
2020-07-24 18:38:32 +0900

    YTG-#8 [메인][모듈] MainFragment 에서 FragmentMainBinding 으로 view inflate 하는 코드 추가

--- features/main/src/main/java/com/az/main/MainFragment.kt
+++ features/main/src/main/java/com/az/main/MainFragment.kt
@@ -1,22 +1,28 @@
 package com.az.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.az.main.databinding.FragmentMainBinding
+import com.az.main.di.loadFeature
 
 class MainFragment : Fragment() {
+    private fun injectFeature() = loadFeature
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        injectFeature()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
