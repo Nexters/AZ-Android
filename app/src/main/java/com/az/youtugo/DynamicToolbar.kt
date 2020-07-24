2364c797d77d387d104c9bd45706432e87c3a617
HunJin Kim <ysg01129@naver.com>
2020-07-25 06:57:57 +0900

    YTG-#10 [툴바][리뷰반영] 코드 개선
    - 중복 코드 제거
    - 불필요한 파일 제거

--- app/src/main/java/com/az/youtugo/DynamicToolbar.kt
+++ app/src/main/java/com/az/youtugo/DynamicToolbar.kt
@@ -2,14 +2,12 @@ package com.az.youtugo
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.annotation.LayoutRes
 import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.android.synthetic.main.backspace_toolbar.view.*
+import kotlinx.android.synthetic.main.default_toolbar.view.*
 import org.koin.core.KoinComponent
 
 class DynamicToolbar @JvmOverloads constructor(
@@ -38,91 +36,74 @@ class DynamicToolbar @JvmOverloads constructor(
     // 로고만 있는 툴바
     private fun setLogoToolbar() {
         removeAllViews()
-        val view = getView(R.layout.default_toolbar)
-        val buttonWrap = view.findViewById<LinearLayout>(R.id.layout_default_button_wrap)
-        val deleteButton = view.findViewById<LinearLayout>(R.id.layout_default_delete_button_wrap)
-        buttonWrap.visibility = View.GONE
-        deleteButton.visibility = View.GONE
-        addView(view)
+        with(getView(R.layout.default_toolbar)) {
+            layout_default_button_wrap.visibility = View.GONE
+            layout_default_delete_button_wrap.visibility = View.GONE
+            addView(this)
+        }
     }
 
     // 로고와 알람, 마이페이지 버튼 툴바
     private fun setDefaultToolbar() {
         removeAllViews()
-        val view = getView(R.layout.default_toolbar)
-        val buttonWrap = view.findViewById<LinearLayout>(R.id.layout_default_button_wrap)
-        val deleteButton = view.findViewById<LinearLayout>(R.id.layout_default_delete_button_wrap)
-        val alarmButton = view.findViewById<ImageView>(R.id.img_alarm)
-        val myPageButton = view.findViewById<ImageView>(R.id.img_my_level)
-
-        buttonWrap.visibility = View.VISIBLE
-        deleteButton.visibility = View.GONE
-
-        alarmButton.setOnClickListener {
-            alarmHandler?.invoke()
+        with(getView(R.layout.default_toolbar)) {
+            layout_default_button_wrap.visibility = View.VISIBLE
+            layout_default_delete_button_wrap.visibility = View.GONE
+            img_alarm.setOnClickListener {
+                alarmHandler?.invoke()
+            }
+            img_my_level.setOnClickListener {
+                myPageHandler?.invoke()
+            }
+            addView(this)
         }
-
-        myPageButton.setOnClickListener {
-            myPageHandler?.invoke()
-        }
-
-        addView(view)
     }
 
     // 로고와 닫기 버튼 툴바
     private fun setLogoCloseToolbar() {
         removeAllViews()
-        val view = getView(R.layout.default_toolbar)
-        val buttonWrap = view.findViewById<LinearLayout>(R.id.layout_default_button_wrap)
-        val closeButton = view.findViewById<LinearLayout>(R.id.layout_default_delete_button_wrap)
-
-        buttonWrap.visibility = View.GONE
-        closeButton.visibility = View.VISIBLE
-
-        closeButton.setOnClickListener {
-            closeHandler?.invoke()
+        with(getView(R.layout.default_toolbar)) {
+            layout_default_button_wrap.visibility = View.GONE
+            layout_backspace_close_button_wrap.apply {
+                visibility = View.GONE
+                setOnClickListener {
+                    closeHandler?.invoke()
+                }
+            }
+            addView(this)
         }
-
-        addView(view)
     }
 
     // 뒤로가기 버튼 툴바
     private fun setBackspaceToolbar(title: String) {
         removeAllViews()
-        val view = getView(R.layout.backspace_toolbar)
-        val txtTitle = view.findViewById<TextView>(R.id.txt_backspace_title)
-        val backspaceButton = view.findViewById<ImageView>(R.id.img_backspace_back_button)
-        val closeButton = view.findViewById<LinearLayout>(R.id.layout_backspace_delete_button_wrap)
-
-        backspaceButton.visibility = View.VISIBLE
-        closeButton.visibility = View.GONE
-        txtTitle.text = title
-
-        backspaceButton.setOnClickListener {
-            backspaceHandler?.invoke()
+        with(getView(R.layout.backspace_toolbar)) {
+            txt_backspace_title.text = title
+            layout_backspace_close_button_wrap.visibility = View.GONE
+            img_backspace_back_button.apply {
+                visibility = View.VISIBLE
+                setOnClickListener {
+                    backspaceHandler?.invoke()
+                }
+            }
+            addView(this)
         }
-
-        addView(view)
     }
 
     // 타이틀과 닫기 버튼 툴바
     private fun setCloseToolbar(title: String) {
         removeAllViews()
-        val view = getView(R.layout.backspace_toolbar)
-        val txtTitle = view.findViewById<TextView>(R.id.txt_backspace_title)
-        val backspaceButton = view.findViewById<ImageView>(R.id.img_backspace_back_button)
-        val closeButton = view.findViewById<LinearLayout>(R.id.layout_backspace_delete_button_wrap)
-
-        closeButton.visibility = View.VISIBLE
-        backspaceButton.visibility = View.GONE
-        txtTitle.text = title
-
-        closeButton.setOnClickListener {
-            closeHandler?.invoke()
+        with(getView(R.layout.backspace_toolbar)) {
+            txt_backspace_title.text = title
+            img_backspace_back_button.visibility = View.GONE
+            layout_backspace_close_button_wrap.apply {
+                visibility = View.VISIBLE
+                setOnClickListener {
+                    closeHandler?.invoke()
+                }
+            }
+            addView(this)
         }
-
-        addView(view)
-
     }
 
     private fun getView(@LayoutRes layoutId: Int): View {
--- app/src/main/java/com/az/youtugo/MainActivity.kt
+++ app/src/main/java/com/az/youtugo/MainActivity.kt
@@ -5,35 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toolbar: DynamicToolbar
-    lateinit var navHostFragment: NavHostFragment
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         initNavigation()
         initToolbarHandler()
     }
 
-    private fun initNavigation() {
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        navController = navHostFragment.findNavController()
-        navController.navigate(R.id.homeFragment)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            toolbar.onDestinationChanged(destination.id, null)
-        }
+    private fun initNavigation() {
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                .findNavController()
+                .apply {
+                    navigate(R.id.homeFragment)
+                    addOnDestinationChangedListener { _, destination, _ ->
+                        toolbar.onDestinationChanged(destination.id, null)
+                    }
+                }
     }
 
     private fun initToolbarHandler() {
