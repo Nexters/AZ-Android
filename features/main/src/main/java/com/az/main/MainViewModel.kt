6ecf7d0597a4c0b7e98e94bbc695424c7a18b1d5
nukeolaf <yhjin04170@gmail.com>
2020-07-28 02:41:37 +0900

    YTG-#8 [메인][레이아웃] fragment_main.xml textView, progressbar ViewModel 과 binding

--- features/main/src/main/java/com/az/main/MainViewModel.kt
+++ features/main/src/main/java/com/az/main/MainViewModel.kt
@@ -1,5 +1,20 @@
 package com.az.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel()
+class MainViewModel : ViewModel() {
+    private val _userGrade = MutableLiveData<String>()
+    val userGrade: LiveData<String> = _userGrade
+    private val _userGradeText = MutableLiveData<String>()
+    val userGradeText: LiveData<String> = _userGradeText
+
+    private val _postCount = MutableLiveData<Int>()
+    val postCount: LiveData<Int> = _postCount
+    private val _commentCount = MutableLiveData<Int>()
+    val commentCount: LiveData<Int> = _commentCount
+
+    private val _humorGradeProgress = MutableLiveData<Int>()
+    val humorGradeProgress: LiveData<Int> = _humorGradeProgress
+}
