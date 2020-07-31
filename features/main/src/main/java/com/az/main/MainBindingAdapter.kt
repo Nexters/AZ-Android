0d074f22316307036ff53c1fcc8935725f0ab942
nukeolaf <yhjin04170@gmail.com>
2020-08-01 01:32:56 +0900

    YTG-#12 [마이페이지][레이아웃] include 한 레이아웃의 rv 에 adapter 가 초기화되지 않아서 임시로 BindingAdapter 에서 adapter 붙이는 코드 추가

--- features/main/src/main/java/com/az/main/MainBindingAdapter.kt
+++ features/main/src/main/java/com/az/main/MainBindingAdapter.kt
@@ -8,5 +8,9 @@ fun setItems(view: RecyclerView, items: List<TempHumorData>?) {
     (view.adapter as? MainHumorsAdapter)?.run {
         items?.let { replaceAll(it) }
         notifyDataSetChanged()
+    } ?: MainHumorsAdapter().apply {
+        view.adapter = this
+        items?.let { replaceAll(it) }
+        notifyDataSetChanged()
     }
 }
