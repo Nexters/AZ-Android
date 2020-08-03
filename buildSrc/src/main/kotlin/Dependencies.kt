86a09d3fc1bcecdf143705919da8c369b3ce20d2
nukeolaf <yhjin04170@gmail.com>
2020-08-04 01:24:40 +0900

    YTG-#15/[상세페이지][모듈] 상세페이지 모듈 - details 모듈 추가

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -98,6 +98,7 @@ object BuildModules {
         const val Home = ":features:home"
         const val Humors = ":features:humors"
         const val MyPage = ":features:mypage"
+        const val Details = ":features:details"
     }
 
     object Libraries {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,7 +34,8 @@ include(
 
     BuildModules.Features.Home,
     BuildModules.Features.Humors,
-    BuildModules.Features.MyPage
+    BuildModules.Features.MyPage,
+    BuildModules.Features.Details
 )
 
 rootProject.name = "Youtugo"
