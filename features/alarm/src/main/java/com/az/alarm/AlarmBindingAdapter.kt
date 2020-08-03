2b97a1796dd315b82b79790250fdbaac831cb49a
nukeolaf <yhjin04170@gmail.com>
2020-08-04 00:06:47 +0900

    YTG-#14/[알림][레이아웃] AlarmViewModel 추가 및 AlarmViewModel DI

---
+++ features/alarm/src/main/java/com/az/alarm/AlarmBindingAdapter.kt
@@ -0,0 +1,12 @@
+package com.az.alarm
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+@BindingAdapter("setItems")
+fun setItems(view: RecyclerView, items: List<TempAlarmData>?) {
+    (view.adapter as? AlarmsAdapter)?.run {
+        items?.let { replaceAll(it) }
+        notifyDataSetChanged()
+    }
+}
--- features/alarm/src/main/java/com/az/alarm/AlarmFragment.kt
+++ features/alarm/src/main/java/com/az/alarm/AlarmFragment.kt
@@ -1,55 +1,49 @@
 package com.az.alarm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.az.alarm.databinding.FragmentAlarmBinding
+import com.az.alarm.di.loadFeature
+import org.koin.android.viewmodel.ext.android.viewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AlarmFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AlarmFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private fun injectFeature() = loadFeature
+    private var _binding: FragmentAlarmBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val viewModel: AlarmViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAlarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        injectFeature()
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            lifecycleOwner = requireActivity()
+            vm = viewModel
+            alarmRv.let {
+                it.adapter = AlarmsAdapter()
+                it.addItemDecoration(getDividerAlarmDecorations())
+            }
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AlarmFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                AlarmFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun getDividerAlarmDecorations(): DividerItemDecoration {
+        return DividerItemDecoration(
+            this@AlarmFragment.context,
+            DividerItemDecoration.VERTICAL
+        ).apply {
+            setDrawable(requireContext().getDrawable(R.drawable.divider_alarm_items)!!)
+        }
     }
 }
