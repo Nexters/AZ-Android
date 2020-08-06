708477d1db4b762d26a6d898955b71f4cfe17c3b
nukeolaf <yhjin04170@gmail.com>
2020-08-06 18:04:01 +0900

    YTG-#16 [글작성페이지][모듈] CreateFragment 시작 시, 키보드 올라오게 하는 코드 추가

--- features/create/src/main/java/com/az/create/CreateFragment.kt
+++ features/create/src/main/java/com/az/create/CreateFragment.kt
@@ -1,9 +1,12 @@
 package com.az.create
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.az.create.databinding.FragmentCreateBinding
 import com.az.create.di.loadFeature
@@ -30,6 +33,19 @@ class CreateFragment : Fragment() {
         binding.apply {
             lifecycleOwner = requireActivity()
             vm = viewModel
+            humorInput.let {
+                it.requestFocus()
+                showSoftInput(it)
+            }
         }
+    }
+
+    private fun showSoftInput(view: EditText) {
+        val inputMethodManager =
+            activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        view.postDelayed(
+            { inputMethodManager.showSoftInput(view, 0) }
+            , 30
+        )
     }
 }
