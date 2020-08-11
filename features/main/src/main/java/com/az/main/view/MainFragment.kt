9766484d0d1bc62517f2a24ece7a80a369f0a542
nukeolaf <yhjin04170@gmail.com>
2020-08-11 21:13:30 +0900

    YTG-#25 [메인][네비게이션] MainFragment 에 데이터 전달 네비 이동 기능 toDetailPage() 추가

--- features/main/src/main/java/com/az/main/view/MainFragment.kt
+++ features/main/src/main/java/com/az/main/view/MainFragment.kt
@@ -4,21 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.az.core.Preferences
 import com.az.main.adapter.MainHumorsAdapter
 import com.az.main.databinding.FragmentMainBinding
 import com.az.main.di.loadFeature
 import com.az.main.viewmodel.MainViewModel
-import com.az.model.Resource
-import com.az.model.Status
-import com.az.model.posts.PostData
-import com.az.model.users.rating.UserRatingData
 import kotlinx.android.synthetic.main.bottom_sheet_main.*
-import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
@@ -46,6 +38,11 @@ class MainFragment : Fragment() {
             humor_card_rv.adapter = MainHumorsAdapter()
             fabCreateHumor.setOnClickListener { toCreatePage.invoke() }
         }
+    }
+
+    private fun toDetailPage(postId: Int) {
+        MainFragmentDirections.actionMainFragmentToDetailsFragment(postId)
+            .let { action -> findNavController().navigate(action) }
     }
 
     private val toCreatePage: () -> Unit = {
