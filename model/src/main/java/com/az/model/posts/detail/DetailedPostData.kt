02ce08f023fea56d65bfec445f21c7961d916dee
nukeolaf <yhjin04170@gmail.com>
2020-08-14 22:36:35 +0900

    YTG-#50 [상세페이지][모델] DetailedPostData 프로퍼티 수정

--- model/src/main/java/com/az/model/posts/detail/DetailedPostData.kt
+++ model/src/main/java/com/az/model/posts/detail/DetailedPostData.kt
@@ -4,12 +4,13 @@ import com.az.model.posts.AuthorData
 
 data class DetailedPostData(
     val author: AuthorData,
-    val bookMarks: Int,
-    val comments: Int,
+    val bookMarkCount: Int,
+    val commentCount: Int,
     val content: String,
     val createdDate: String,
     val id: Int,
     val likes: Int,
     val modifiedDate: String,
+    val pressBookMark: Boolean,
     val pressLike: Boolean
 )
