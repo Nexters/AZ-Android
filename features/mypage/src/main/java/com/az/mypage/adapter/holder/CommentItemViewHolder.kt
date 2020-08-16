e8406e984c6835a8881da14f5d6ad0b4277e0c66
HunJin Kim <ysg01129@naver.com>
2020-08-16 14:33:11 +0900

    YTG-#12 [마이페이지][탭별 아이템] 탭별 아이템 UI 개발
    - 작성 글, 북마크 아이템
    - 댓글 아이템
    - 설정 아이템

---
+++ features/mypage/src/main/java/com/az/mypage/adapter/holder/CommentItemViewHolder.kt
@@ -0,0 +1,2 @@
+package com.az.mypage.adapter.holder
+
---
+++ features/mypage/src/main/java/com/az/mypage/adapter/holder/HumorCardItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.az.mypage.adapter.holder
+
+import androidx.recyclerview.widget.RecyclerView
+
+class HumorCardItemViewHolder: RecyclerView.ViewHolder() {
+
+}
