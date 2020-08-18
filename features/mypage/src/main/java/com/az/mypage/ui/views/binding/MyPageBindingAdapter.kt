7f31273ee431c45627f3160f66ad20c574c253b5
HunJin Kim <ysg01129@naver.com>
2020-08-19 01:53:07 +0900

    YTG-#61 [마이페이지][스티커] 스티커 적용

--- features/mypage/src/main/java/com/az/mypage/ui/views/binding/MyPageBindingAdapter.kt
+++ features/mypage/src/main/java/com/az/mypage/ui/views/binding/MyPageBindingAdapter.kt
@@ -1,8 +1,11 @@
 package com.az.mypage.ui.views.binding
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.az.model.BaseDataInterface
+import com.az.model.users.rating.Rating
+import com.az.mypage.R
 import com.az.mypage.adapter.MyPageRecyclerViewAdapter
 import com.az.mypage.model.MyPageItemCode
 
@@ -12,7 +15,7 @@ fun setItems(view: RecyclerView, items: List<BaseDataInterface>?) {
         items?.let {
             replaceAll(it)
             notifyDataSetChanged()
-        }
+        } ?: replaceAll(listOf())
     }
 }
 
@@ -21,4 +24,15 @@ fun setViewType(view: RecyclerView, viewType: MyPageItemCode) {
     (view.adapter as MyPageRecyclerViewAdapter).run {
         this.changeViewType(viewType)
     }
+}
+
+@BindingAdapter("gradeForUserSticker")
+fun setUserStickerWithGrade(view: ImageView, code: String?) {
+    return when (code) {
+        Rating.ASSISTANT_MANAGE.code -> view.context.getDrawable(R.drawable.ic_daeri_full)
+        Rating.DEPARTMENT_HEAD.code -> view.context.getDrawable(R.drawable.ic_bujang_full)
+        Rating.MANAGING_DIRECTOR.code -> view.context.getDrawable(R.drawable.ic_sangmu_full)
+        Rating.BOSS.code -> view.context.getDrawable(R.drawable.ic_sajang_full)
+        else -> view.context.getDrawable(R.drawable.ic_sinip_full)
+    }.let { view.setImageDrawable(it) }
 }
