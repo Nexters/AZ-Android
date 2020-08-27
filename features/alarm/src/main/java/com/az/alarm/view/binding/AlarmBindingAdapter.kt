008460bf75a33f575c23073434dd0ed0244164d8
nukeolaf <yhjin04170@gmail.com>
2020-08-27 18:09:38 +0900

    YTG-#74 [알림페이지][아이템] 알림페이지 알림타입 보여주는 기능 추가

--- features/alarm/src/main/java/com/az/alarm/view/binding/AlarmBindingAdapter.kt
+++ features/alarm/src/main/java/com/az/alarm/view/binding/AlarmBindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.az.alarm.view.binding
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.az.alarm.adapter.AlarmsAdapter
@@ -11,4 +12,13 @@ fun setItems(view: RecyclerView, items: List<DetailedNoticeData>?) {
         items?.let { replaceAll(it) } ?: replaceAll(listOf())
         notifyDataSetChanged()
     }
+}
+
+@BindingAdapter("noticeType")
+fun setNoticeType(view: TextView, type: String?) {
+    return when (type) {
+        "COMMENT" -> "댓글"
+        "LIKE" -> "좋아요"
+        else -> "error"
+    }.let { view.text = it }
 }
